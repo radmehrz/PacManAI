d453a7a5a2cb117beed2d2e66c5ccfcf047e6520
radmehrz <58809469+radmehrz@users.noreply.github.com>
2020-01-20 16:58:28 +0330

    add map reader from selfplay mode

--- PacManAi.c
+++ PacManAi.c
@@ -25,3 +25,63 @@ typedef struct star{
 	int x;
 	int y;
 }Star;
+
+
+
+enum key{up,down,left,right,enter,esc};
+enum option{aiplay,selfplay,settings,quit};
+
+point map[5][10];
+int X;
+int Y;
+Star star; 
+
+void init_map(int maxX, int maxY){
+	char temp;
+	FILE *fptr;
+	char filedir[200];
+	printf("Enter file directory: ");
+	scanf("%s" , filedir);
+	fptr = fopen(filedir,"r");
+	while(fptr == NULL)
+	{
+		printf("Enter file directory: ");
+		scanf("%s" , filedir);
+		fptr = fopen(filedir,"r");
+	}
+	for(int i = 0;i < maxX;i++)
+	for(int j = 0;j < maxY;j++)
+	{
+		while ((temp = getc(fptr)) != EOF)
+		{
+			if(temp == '0')
+			{
+				map[i][j].data=cEmpty;
+				X = i;
+				Y = j;
+				break;
+			}
+			else if(temp == '1')
+			{
+				map[i][j].data= cEmpty;
+				break;
+			}
+			else if(temp == '#')
+			{
+				map[i][j].data= cBlock;
+				break;
+			}
+			else if(temp == '*')
+			{
+				map[i][j].data= cFruit;
+				break;
+			}
+			else continue;
+		}
+		
+		map[i][j].cx = -1;
+		map[i][j].cy = -1;
+		map[i][j].visited = false;
+	}
+	fclose(fptr);
+}
